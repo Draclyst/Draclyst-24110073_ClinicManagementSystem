074e061a150dac1e126eac23de949165904d7cfb
Draclyst <115409932+Draclyst@users.noreply.github.com>
2025-09-10 14:35:51 +0700

    Update ClinicManagementNew.cpp
    Implemented revisit date for appointments, medical prescription and appointment costs

--- ClinicManagementNew.cpp
+++ ClinicManagementNew.cpp
@@ -1,12 +1,14 @@
 #include <string>
 #include <iostream>
 using namespace std;
-
+class Prescription;
 class Patient{
     protected:
     string name; //atributes
     int ID;
     int age;
+    string revisitDate;
+    Prescription* prescription;
     public:
     Patient(string n, int id, int a): name(n), ID(id), age(a) {}
     void displayInfo(){ //prints patient's info
@@ -18,8 +20,21 @@ class Patient{
     string getName() { //retrieves patient's name
          return name;
     }
+    void setRevisitDate(string date) { //sets new visit date
+        revisitDate=date;
+    }
+    string getRevisitDate() {
+        return revisitDate;
+    }
+    void setPrescription(Prescription* p){
+        prescription = p;
+    }
+    Prescription* getPrescription() {
+        return prescription;
+    }
 };
 class SpecializedPatient : public Patient{
+    protected:
     string conditionType; //adds special condition details
     public:
     SpecializedPatient(string n, int id, int a , string ctype): Patient(n, id, a), conditionType(ctype) {
@@ -41,6 +56,11 @@ class Appointment{
     void displayInfo(); //prints appointment details
     void updateStatus(string newStatus){ //updates appointment status
         status = newStatus;
+    }
+    double appointmentCost(){ //cost of each appointment
+        if(status == "Completed") return 100.0;
+        else if(status == "Available")
+        return 0.0;
     }
 };
 class Doctor{
@@ -67,13 +87,26 @@ class Doctor{
 };
 void Appointment::displayInfo(){ //prints appointment details
     cout<<"Date: "<<date<<", Time: "<<time<<", Reason: "<<reason<<", Patient: "<<patient->getName()<<", Doctor: "<<doctor->getName()<<", Status: "<<status<<endl;
+    cout<<"Cost: "<<appointmentCost()<< " $"<<endl;
+    if(status=="Completed"){
+    cout<<"Revisit Date: "<<patient->getRevisitDate()<<endl;
+    }
 }
-
+class Prescription{
+    string medication,dosage,instructions;
+    public:
+    Prescription(string m, string d, string i): medication(m), dosage(d), instructions(i) {}
+    void displayInfo(){
+        cout<<"Prescription: "<<medication<<", Dosage: "<<dosage<<", Instructions: "<<instructions<<endl;
+    }
+};
 int main(){
     Doctor doctor1("Dr. Slime",420,"Neurology");
     Doctor doctor2("Dr. Pork",1337,"Cardiology");
     Patient patient1("John Roblox",056, 26);
     SpecializedPatient patient2("Jane Minecraft",682, 46,"Obesity");
+    patient1.setRevisitDate("15-09-2026");
+    patient2.setRevisitDate("09-12-2025");
     patient1.displayInfo();
     patient2.displayInfo();
     Appointment a1("08-09-2025", "3:00 PM", "General Checkup", &patient1, &doctor1);
